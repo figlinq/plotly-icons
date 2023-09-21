c33542d5cbb556f7a01a5dc2ba2134c49049965e
dmt0 <dmitry@plot.ly>
2023-09-20 21:54:47 -0400

    Regression fix

--- src/components/TraceTypeTreemapIcon.js
+++ src/components/TraceTypeTreemapIcon.js
@@ -3,7 +3,7 @@ import PlotlyIconBase from './IconBase.js';
 
 const TraceTypeTreemapIcon = (props) => (
   <PlotlyIconBase viewBox="0 0 60 60" {...props}>
-    <path fill="#08507a" d="M10.1 11.2h19.4v39.1H10.1z" className="st0"></path><path fill="#55bbe7" d="M30.6 11.2h19.6v21.5H30.6z" class="st1"></path><path fill="#2477A2" d="M30.6 33.7h19.6v16.7H30.6z" class="st2"></path>
+    <path fill="#08507a" d="M10.1 11.2h19.4v39.1H10.1z" className="st0"></path><path fill="#55bbe7" d="M30.6 11.2h19.6v21.5H30.6z" className="st1"></path><path fill="#2477A2" d="M30.6 33.7h19.6v16.7H30.6z" className="st2"></path>
   </PlotlyIconBase>
 );
 
